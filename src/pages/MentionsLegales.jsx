3c0b31fa4ee481b0d4dc33f4c2f3f73d5cabcb10
alexandrebdry <alexbaudry0@gmail.com>
2023-04-13 13:56:17 +0200

    Nav trop stylé

---
+++ src/pages/MentionsLegales.jsx
@@ -0,0 +1,21 @@
+export default function () {
+
+    return (
+        <div className={"h-screen px-4 pt-36 bg-primary"}>
+            <h1 className={"text-xl font-bold py-2"}>Mentions légales</h1>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium adipisci alias aliquid atque
+                consequatur cumque, cupiditate delectus deleniti dolor doloremque doloribus ducimus earum eius enim
+                error est et eum excepturi expedita facilis fuga fugiat fugit harum id illum impedit incidunt ipsa
+                ipsam ipsum iste iure iusto laboriosam laborum laudantium libero magnam magni maxime minima minus
+                molestiae mollitia nam natus nemo neque nisi nobis non nostrum nulla numquam obcaecati odit officia
+                officiis omnis optio pariatur perferendis perspiciatis placeat praesentium provident quae quaerat
+                quam quas qui quibusdam quidem quis quisquam quo quod quos recusandae repellat reprehenderit rerum
+                saepe sapiente sed sequi similique sit soluta suscipit tempora temporibus tenetur totam ullam unde
+                ut velit veniam veritatis vero voluptas voluptatem voluptates voluptatibus voluptatum. Accusamus
+                adipisci alias aliquid amet animi architecto asperiores atque autem beatae blanditiis commodi
+            </p>
+
+        </div>
+    )
+}
