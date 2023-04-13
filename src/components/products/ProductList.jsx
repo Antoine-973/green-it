dc4c4ed4162b173e259e853485719af87163cc23
Alexandrebdry <alexbaudry0@gmail.com>
2023-04-13 21:37:13 +0200

    Carousel

--- src/components/products/ProductList.jsx
+++ src/components/products/ProductList.jsx
@@ -5,6 +5,7 @@ export function ProductList() {
 
     return (
         <>
+            {/*
             <div className={"flex flex-col items-center w-3/4"}>
                 <div className="w-64 sm:w-2/3 sm:h-96 carousel sm:carousel-vertical rounded-box">
                     {products.map((product) => (
@@ -38,6 +39,48 @@ export function ProductList() {
 
 
             </div>
+            */}
+            <h2 className={"text-secondary font-bold tracking-widest"}>VOITURES</h2>
+            <div className="flex justify-center w-full py-2 gap-2">
+                {products.map((product, key) => (
+                    <a href={"#slide" + key} className="btn btn-xs">{key+1}</a>
+                ))}
+            </div>
+
+            <div className="carousel w-full">
+                {products.map((product, key) => (
+
+                <div id={"slide" + key} className="carousel-item h-[60vh] sm:h-64 flex flex-col sm:flex-row overflow-y-hidden  relative w-full">
+                    <div className={"flex flex-col items-center w-full justify-center gap-4"}>
+                        <h1 className={"font-bold text-3xl font-bold italic"}>{product.model}</h1>
+                        <p className={"font-thin"}>à partir de {product.price} €</p>
+                        <Link className={"block px-4 py-2 btn-clip bg-secondary text-white text-center"} href={"/product/" + product.id}>
+                            Acheter maintenant
+                        </Link>
+                    </div>
+                    <img
+                        className={"z-10 relative"}
+                        src={product.image}
+                        alt={product.model + " image"}
+                        width={500}
+                        height={280}
+                    />
+                    <picture className={"z-0 absolute right-0 sm:right-4 -bottom-24 sm:-bottom-28 overflow-x-hidden "} >
+                        <source media={"(min-width: 640px)"} srcSet={"pied.webp"}  type={"image/webp"}/>
+                        <img
+                            className={"z-0"}
+                            src={"piedistallo-2x.webp"}
+                            alt={"Piedestal image"}
+                            width={350}
+                            height={200}/>
+                    </picture>
+
+                </div>
+                ))}
+
+            </div>
+
+
         </>
     )
 }
--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -7,10 +7,10 @@ export function Products() {
             <div className={"grid grid-cols-1 p-8 gap-8 text-center pt-32 w-full h-full"}>
                 <h1 className={"text-xl font-extrabold italic"}>CONSTITUEZ VOTRE COLLECTION</h1>
                 <p>
-                    Acheter et collectionnez plus de 60 véhicules parmi les plus rapides et les plus puissants de l'industrie.
-                    Découvrez leurs attributs, leur personnalité, leur style de conduite.
+                    Acheter et collectionnez plus de <strong>60 véhicules</strong> parmi les plus rapides et les plus puissants de l'industrie.
+                    Découvrez leurs attributs, leur personnalité, leur style de conduite. Nos modèles vous permetteront de vivre des expériences uniques et d'atteindre des vitesses folles.
                 </p>
-                <h2 className={"text-secondary font-bold tracking-widest"}>VOITURES</h2>
+
             </div>
             <div className={"grid grid-cols-1 justify-items-center"}>
                 <ProductList />
